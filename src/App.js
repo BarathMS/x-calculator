3d1331c2b699740b535c67fdf3267b184a13e48e
Barat shankar <barathms96@gmail.com>
2025-03-22 18:42:57 +0530

    calculator completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const handleButtonClick = (value) => {
+    if (value === "c") {
+      setInput("");
+      setResult("");
+    } else if (value === "=") {
+      try {
+        setResult(eval(input));
+        // The eval() [evaluate] function in JavaScript is a built-in function that evaluates a string as JavaScript code
+        // (using try-catch is a better option) when using eval() function 
+      } catch (error) {
+        setResult("Error");
+      }
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <input style={{color:"grey", borderColor:"transparent"}} type="text" value={result} readOnly />
+      </div>
+      <div className="button-container">
+        <button onClick={() => handleButtonClick(7)}>7</button>
+        <button onClick={() => handleButtonClick(8)}>8</button>
+        <button onClick={() => handleButtonClick(9)}>9</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
+        <button onClick={() => handleButtonClick(4)}>4</button>
+        <button onClick={() => handleButtonClick(5)}>5</button>
+        <button onClick={() => handleButtonClick(6)}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
+        <button onClick={() => handleButtonClick(1)}>1</button>
+        <button onClick={() => handleButtonClick(2)}>2</button>
+        <button onClick={() => handleButtonClick(3)}>3</button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => handleButtonClick("c")}>c</button>
+        <button onClick={() => handleButtonClick(0)}>0</button>
+        <button onClick={() => handleButtonClick("=")}>=</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+      </div>
     </div>
   );
 }
